8731b2f5377217c493d462edf8f013b49b1d3534
Sam <samuel.dacosta@student.manchester.ac.uk>
2018-09-02 15:45:59 +0100

    Finish repo service layer tests

--- src/test/java/com/littlegit/server/repo/service/RepoServiceTests.kt
+++ src/test/java/com/littlegit/server/repo/service/RepoServiceTests.kt
@@ -2,6 +2,8 @@ package com.littlegit.server.repo.service
 
 import com.littlegit.server.application.exception.DuplicateRecordException
 import com.littlegit.server.model.repo.CreateRepoModel
+import com.littlegit.server.model.repo.RepoAccessLevel
+import com.littlegit.server.model.user.AuthRole
 import com.littlegit.server.repo.GitServerRepository
 import com.littlegit.server.repo.RepoAccessRepository
 import com.littlegit.server.repo.RepoRepository
@@ -13,7 +15,9 @@ import com.littlegit.server.service.RepoService
 import littlegitcore.LittleGitCoreWrapper
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mockito.mock
+import org.mockito.ArgumentMatchers
+import org.mockito.Mockito.*
+import java.nio.file.Paths
 
 class RepoServiceTests {
 
@@ -53,5 +57,66 @@ class RepoServiceTests {
         upon(repoRepoMock.getRepoByNameAndCreator(user, repoName)).thenReturn(RepoHelper.createTestRepo(name = repoName, user = user))
 
         repoService.createRepo(user, CreateRepoModel(repoName))
+        verify(repoRepoMock, times(0)).createRepo(ArgumentMatchers.any(), user, ArgumentMatchers.anyString(), ArgumentMatchers.anyInt())
+    }
+
+    @Test
+    fun testCreateRepo_AsBasicUser_IsSuccessful() {
+        val user = UserHelper.createTestUser(authRole = AuthRole.BasicUser)
+        val repoName = "PlansForMoria"
+        val createModel = CreateRepoModel(repoName)
+        val server = GitServerHelper.createGitServer()
+        val repoId = 1
+        val cloneUrlPath = "${user.username}/$repoName"
+
+        upon(gitServerRepository.getBestGitServerForUser(user)).thenReturn(server)
+        upon(repoRepoMock.getRepoByNameAndCreator(user, repoName)).thenReturn(null)
+        upon(repoRepoMock.createRepo(createModel, user, cloneUrlPath, server.id)).thenReturn(repoId)
+        upon(littleGitCoreWrapper.initRepo(user, createModel, server)).thenReturn(Paths.get(cloneUrlPath))
+
+        repoService.createRepo(user, createModel)
+
+        verify(repoRepoMock, times(repoId)).getRepoSummary(repoId)
+        verify(repoAccessRepoMock, times(repoId)).grantRepoAccess(user, repoId, RepoAccessLevel.Contributor)
+    }
+
+    @Test
+    fun testCreateRepo_AsAdmin_IsSuccessful() {
+        val user = UserHelper.createTestUser(authRole = AuthRole.Admin)
+        val repoName = "PlansForMoria"
+        val createModel = CreateRepoModel(repoName)
+        val server = GitServerHelper.createGitServer()
+        val repoId = 1
+        val cloneUrlPath = "${user.username}/$repoName"
+
+        upon(gitServerRepository.getBestGitServerForUser(user)).thenReturn(server)
+        upon(repoRepoMock.getRepoByNameAndCreator(user, repoName)).thenReturn(null)
+        upon(repoRepoMock.createRepo(createModel, user, cloneUrlPath, server.id)).thenReturn(repoId)
+        upon(littleGitCoreWrapper.initRepo(user, createModel, server)).thenReturn(Paths.get(cloneUrlPath))
+
+        repoService.createRepo(user, createModel)
+
+        verify(repoRepoMock, times(repoId)).getRepoSummary(repoId)
+        verify(repoAccessRepoMock, times(repoId)).grantRepoAccess(user, repoId, RepoAccessLevel.Owner)
+    }
+
+    @Test
+    fun testCreateRepo_AsOrganizationAdmin_IsSuccessful() {
+        val user = UserHelper.createTestUser(authRole = AuthRole.OrganizationAdmin)
+        val repoName = "PlansForMoria"
+        val createModel = CreateRepoModel(repoName)
+        val server = GitServerHelper.createGitServer()
+        val repoId = 1
+        val cloneUrlPath = "${user.username}/$repoName"
+
+        upon(gitServerRepository.getBestGitServerForUser(user)).thenReturn(server)
+        upon(repoRepoMock.getRepoByNameAndCreator(user, repoName)).thenReturn(null)
+        upon(repoRepoMock.createRepo(createModel, user, cloneUrlPath, server.id)).thenReturn(repoId)
+        upon(littleGitCoreWrapper.initRepo(user, createModel, server)).thenReturn(Paths.get(cloneUrlPath))
+
+        repoService.createRepo(user, createModel)
+
+        verify(repoRepoMock, times(repoId)).getRepoSummary(repoId)
+        verify(repoAccessRepoMock, times(repoId)).grantRepoAccess(user, repoId, RepoAccessLevel.Owner)
     }
 }
