79adfea29ad01cd0894de0ede28e31298feade52
Sam <samuel.dacosta@student.manchester.ac.uk>
2018-09-10 09:05:59 +0100

    Finish the restrict commands script

--- git-server-scripts/restrict-commands.sh
+++ git-server-scripts/restrict-commands.sh
@@ -4,15 +4,18 @@ userId=$1
 regex="git-[a-z]+-pack '(.*)'"
 apiKey="0987654321"
 serverUrlBase="http://localhost:6122"
-sshCommand="$SSH_ORIGINAL_COMMAND"
 
-if [[ ${sshCommand} =~ $regex ]]
+if [[ "$SSH_ORIGINAL_COMMAND" =~ $regex ]]
    then
        repoPath="${BASH_REMATCH[1]}"
    else
        exit 1
 fi
-echo  "$serverUrlBase/repo/check-user-access?userId=$userId&repoPath=$repoPath"
-curl -X GET "$serverUrlBase/repo/check-user-access?userId=$userId&repoPath=$repoPath" -H 'x-api-key: 0987654321'
 
-exec git-shell -c "$sshCommand"
+hasAccess=$(curl -s -X GET "$serverUrlBase/repo/check-user-access?userId=$userId&repoPath=$repoPath" -H "x-api-key: $apiKey")
+if [[ "$hasAccess" = "true" ]]
+    then
+        exec git-shell -c "$SSH_ORIGINAL_COMMAND"
+    else
+        exit 1
+fi
